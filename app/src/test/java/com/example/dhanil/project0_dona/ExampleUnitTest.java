2d413d25e27811dc0f01a1b1828ccbbb4f72fcba
Dona Rose Thomas <missdonarose@yahoo.co.uk>
2015-10-29 10:09:32 +0300

    First commit

---
+++ app/src/test/java/com/example/dhanil/project0_dona/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.dhanil.project0_dona;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
